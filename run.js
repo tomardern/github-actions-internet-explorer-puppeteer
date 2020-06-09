d6a42fafab68f717d7845070f1e6f9cfd4c118f3
Tom Ardern <tomardern@bloomandwild.com>
2020-06-09 11:28:32 +0100

    Run with

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer-ie");
 (async () => {
   const browser = await puppeteer.launch({});
   const page = await browser.newPage();
-  await page.goto("https://www.bloomandwild.com/");
+  await page.goto("https://tsc:tsc@staging.traderssupportclub.com/");
   const title = await page.title();
   console.log("Title is", title);
   await page.close();
