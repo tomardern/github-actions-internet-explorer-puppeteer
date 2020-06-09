dadca6b98b323aff1783f57bc28e1ab1c89633b4
Tom Ardern <tomardern@bloomandwild.com>
2020-06-09 11:10:39 +0100

    Forcing

--- run.js
+++ run.js
@@ -1,13 +1,12 @@
 const puppeteer = require("puppeteer-ie");
 
-(async () => {
-  const browser = await puppeteer.launch({});
-  const page = await browser.newPage();
-  await page.goto("https://www.bloomandwild.com/");
-  const title = await page.title();
-  console.log("Title is", title);
-  await page.close();
-  console.log("Page is closed");
-  await browser.close();
-  console.log("Browser is closed");
-})();
+const browser = await puppeteer.launch({});
+const page = await browser.newPage();
+await page.goto("https://www.bloomandwild.com/");
+const title = await page.title();
+console.log("Title is", title);
+await page.close();
+console.log("Page is closed");
+await browser.close();
+console.log("Browser is closed");
+process.exit(1);
